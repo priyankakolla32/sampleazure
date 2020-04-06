13724da997c3a303522a717202f22aa634eb87ea
gkolluri <gkolluri@MC142.corp.miraclesoft.com>
2020-04-06 19:39:07 +0530

    INITIAL COMMIT1

---
+++ sampleazure/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@Autowired
+	ServiceEx serviceEx;
+	
+	@GetMapping("/get")
+	public String method1()
+	{
+		return "HAI EVERYONE";
+	}
+	
+//	@GetMapping("/get")
+//	public String method2()
+//	{
+//		return serviceEx.getData();
+//	}
+	
+	@GetMapping("/getString/{name}/{city}")
+	public String getString(@PathVariable String name, @PathVariable String city) {
+		return serviceEx.getString(name, city);
+	}
+
+	@PostMapping("/getObject")
+	public String getObject(@RequestBody BeanEx exampleBean) {
+		return serviceEx.getObject(exampleBean);
+	}
+
+
+
+	
+}
---
+++ sampleazure/src/main/java/com/example/demo/ServiceEx.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+
+
+public interface ServiceEx {
+
+//	public String getData() {
+//		
+//		
+//	}
+
+	public String getString(String name, String city);
+
+	public String getObject(BeanEx exampleBean);
+	
+	
+	
+}
