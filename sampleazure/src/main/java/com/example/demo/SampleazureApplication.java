df7901f0d97871f1fef72d3c8701e712154a99c8
gkolluri <gkolluri@MC142.corp.miraclesoft.com>
2020-04-07 22:31:23 +0530

    COMMIT 4

--- sampleazure/src/main/java/com/example/demo/SampleazureApplication.java
+++ sampleazure/src/main/java/com/example/demo/SampleazureApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
@@ -13,12 +14,12 @@ public class SampleazureApplication {
 	}
 
 	@Controller
-	public class HomeController {
-	    @RequestMapping("/home")
-	    public String home(){
-	        return "home";
-	    }
+	class WebController {
+		
+		@GetMapping
+		public String home(){
+			return "index";
+		}
 	}
-	
 	
 }
