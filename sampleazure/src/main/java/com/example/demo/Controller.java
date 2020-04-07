2ff9b1df458eda6f9e7b0e567204f48278f00470
gkolluri <gkolluri@MC142.corp.miraclesoft.com>
2020-04-07 16:53:03 +0530

    THIRD COMMIT

--- sampleazure/src/main/java/com/example/demo/Controller.java
+++ sampleazure/src/main/java/com/example/demo/Controller.java
@@ -8,9 +8,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("/api")
 public class Controller {
+    @RequestMapping("/web")
+    public String home(){
+        return "home";
+    }
 
 	@Autowired
 	ServiceEx serviceEx;
--- sampleazure/src/main/java/com/example/demo/SampleazureApplication.java
+++ sampleazure/src/main/java/com/example/demo/SampleazureApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class SampleazureApplication {
@@ -10,4 +12,13 @@ public class SampleazureApplication {
 		SpringApplication.run(SampleazureApplication.class, args);
 	}
 
+	@Controller
+	public class HomeController {
+	    @RequestMapping("/home")
+	    public String home(){
+	        return "home";
+	    }
+	}
+	
+	
 }
